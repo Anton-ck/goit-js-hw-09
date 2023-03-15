f35afcbeb0958a7b29230c418850fd090c63879e
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-03-15 12:34:14 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => { 
+    timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+}, 1000);  
+});
+
+stopBtn.addEventListener('click', () => { 
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+})
